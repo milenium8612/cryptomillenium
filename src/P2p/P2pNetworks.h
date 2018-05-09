c743873f81a98bd8a0fb9a75cfa5a6af8efd7d07
milenium8612 <mileniummail8612@gmail.com>
2018-05-09 12:17:56 +0300

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x48, 0x59, 0x57, 0x51, 0x55, 0x8f, 0x2e, 0x5f, 0x85, 0x35, 0x00, 0x15, 0x17, 0x18, 0x08, 0x50 } };
+}
